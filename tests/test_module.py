82391281fb002fcf28734b1df6c4004234ea294a
Stephanos Kuma <spapanik21@gmail.com>
2020-08-17 17:02:05 +0300

    Add test mocking a builtin

---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+from unittest import mock
+
+from mocking import module
+
+
+@mock.patch("mocking.module.print", new_callable=mock.MagicMock)
+def test_mock_a_builtin(mock_print):
+    assert mock_print.call_count == 0
+    module.call_print_builtin()
+    assert mock_print.call_count == 1
+    assert mock_print.call_args_list == [mock.call("actual string")]
