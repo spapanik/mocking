f04e8cce2e9a713b9701e5ab4f6a72ff89b882ea
Stephanos Kuma <spapanik21@gmail.com>
2020-08-18 12:02:58 +0300

    Add test mocking a function from the same module

--- src/mocking/module.py
+++ src/mocking/module.py
@@ -1,2 +1,10 @@
+def echo(*args):
+    return args
+
+
 def call_print_builtin():
     print("actual string")
+
+
+def call_same_module_function(*args):
+    return echo(*args)
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,5 +1,7 @@
 from unittest import mock
 
+import pytest
+
 from mocking import module
 
 
@@ -9,3 +11,37 @@ def test_mock_a_builtin(mock_print):
     module.call_print_builtin()
     assert mock_print.call_count == 1
     assert mock_print.call_args_list == [mock.call("actual string")]
+
+
+@mock.patch("mocking.module.echo", return_value=42)
+def test_mock_a_function_same_module_constant_return(mock_echo):
+    assert module.call_same_module_function() == 42
+
+
+@mock.patch("mocking.module.echo", new=mock.MagicMock(return_value=42))
+def test_mock_a_function_same_module_constant_return_no_arg():
+    assert module.call_same_module_function() == 42
+
+
+@mock.patch("mocking.module.echo", side_effect=[42, 1024])
+def test_mock_a_function_same_module_return_from_a_sequence(mock_echo):
+    assert module.call_same_module_function() == 42
+    assert module.call_same_module_function() == 1024
+    assert pytest.raises(StopIteration, module.call_same_module_function)
+
+
+@mock.patch("mocking.module.echo", new=mock.MagicMock(side_effect=[42, 1024]))
+def test_mock_a_function_same_module_return_from_a_sequence_no_arg():
+    assert module.call_same_module_function() == 42
+    assert module.call_same_module_function() == 1024
+    assert pytest.raises(StopIteration, module.call_same_module_function)
+
+
+@mock.patch("mocking.module.echo")
+def test_mock_a_function_same_module_return_from_function(mock_echo):
+    def mock_echo_side_effect(argument):
+        return 42 if argument else 1024
+    mock_echo.side_effect = mock_echo_side_effect
+    assert module.call_same_module_function(True) == 42
+    assert module.call_same_module_function(False) == 1024
+    assert pytest.raises(TypeError, module.call_same_module_function)
